4852f903d88668199f2eb7397067a2f9657063ba
Brandon Jones <orthofid013@gmail.com>
2024-11-30 17:44:09 -0700

    Add additional lesson work

---
+++ Lesson16.6_Quiz_2/Lesson16.6_Quiz_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lesson16.6_Quiz_2
+//
+//  Created by Brandon Jones on 11/30/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
